1cb7df131e8091c89831cfe7f1af144093e878da
dj-Andres <andres96jimenez@gmail.com>
2020-08-05 21:23:52 -0500

    importando librerias y usando reactdom y uso de variables y funciones

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//ejemplo de usar reacj reactdom
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//1--importar librerias
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from 'react'
+import reactDOM from 'react-dom'
+
+//const element=<h1>Hola mundo a todos XD...</h1>
+//const container=document.getElementById('root')
+//2------ reactDOM.render(que___donde)
+//reactDOM.render(element,container)
+
+//3----introducir una variable
+//const elemento=<h1>Hola mi nombre es: {nombre}</h1>
+//4-----ejempplo de funcion 
+
+function obtenerusuario(usuario){
+  return usuario.name+ '  ' +usuario.apellido;
+}
+const usuario={
+  name:'Andres',
+  apellido:'Jimenez'
+}
+const elemento=<h1>Hola mi nombre es: {obtenerusuario(usuario)}</h1>
+const contenedor=document.getElementById('root')
+reactDOM.render(elemento,contenedor)
