c23eb5f995a4d0cbfe056b35512ac29173b8bce2
dj-Andres <andres96jimenez@gmail.com>
2020-08-13 22:00:29 -0500

    agregando estilos css y bootstap

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,28 @@
 //creando componentes en este caso componente de clase//
 import React from 'react'
+import ejemplo from '../img/descarga.jpg'
+import circulo from '../img/images.jpg'
+import './style/card.css'
 //herencia de component//
 class Card extends React.Component{
     render(){
-        return <h1>Card</h1>
+        const{title,description,img,leftColor,rightColor}=this.props
+        return (
+                <div className="Card mx-auto fitnes-Card" style={{backgroundImage:`url(${circulo}),linear-gradient(to right, ${leftColor}, ${rightColor})`}}>
+                    <div className="card-body">
+                        <div className="row center">
+                            <div className="col-6">
+                                <img src={img} className="float-right foto"/>
+                            </div>
+                            <div className="col-6 fitnes-Card-info">
+                                <h1>{title}</h1> 
+                                <p>{description}</p>
+                            </div>
+                        </div>        
+                    </div>
+                </div>
+        )
     }
 }
+//exportar la clase //
 export default Card
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import React from 'react'
 import reactDOM from 'react-dom'
 //como importar la clase de componentes
 import Card from './components/card'
+import 'bootstrap/dist/css/bootstrap.css'
 //const element=<h1>Hola mundo a todos XD...</h1>
 //const container=document.getElementById('root')
 //2------ reactDOM.render(que___donde)
@@ -28,4 +29,11 @@ import Card from './components/card'
 
 //ejecutar los componentes
 const contenedor=document.getElementById('root')
-reactDOM.render(<Card/>,contenedor)
+//agg propiedades a los componentes//
+reactDOM.render(<Card
+    title="Mi Titulo"
+    description="Ejemplo de uso"
+    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+    leftColor="#A74CF2"
+    rightColor="#617BFB"
+/>,contenedor)
